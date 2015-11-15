2265b4336659fd29bdbb2384157f13af8f775a38
Ryan Marcus <ryan@rmarcus.info>
2015-11-15 12:41:14 -0500

    no more dynamic loading

--- util/dynamic_loader.js
+++ util/dynamic_loader.js
@@ -4,39 +4,44 @@
 
 
 let FS = require("q-io/fs");
+let Q = require("q");
 
+let readers = {
+	"csv": require("../read/csv.js"),
+	"json": require("../read/json.js"),
+	"xlsx": require("../read/xlsx.js"),
+	"xml": require("../read/xml.js")
+};
+
+let writers = {
+	"csv": require("../write/csv.js"),
+	"gv": require("../write/gv.js"),
+	"json": require("../write/json.js"),
+	"pandat": require("../write/pandat.js"),
+	"sql": require("../write/sql.js"),
+	"xml": require("../write/xml.js")
+};
 
 
 module.exports.getReaders = getReaders;
 function getReaders() {
-	return FS.list("read/").then((files) => {
-		return files
-			.filter(f => f.endsWith("js"))
-			.map((f) => f.split(".")[0]);
-	});
+	return Q(Object.keys(readers));
 }
 
 
 module.exports.getWriters = getWriters;
 function getWriters() {
-	return FS.list("write/").then((files) => {
-		return files
-			.filter(f => f.endsWith("js"))
-			.map((f) => f.split(".")[0]);
-	});
+	return Q(Object.keys(writers));
 }
 
 
 module.exports.getReader = getReader;
 function getReader(reader) {
-	// TODO loading arbitrary JS!
-	var toR = require("../read/" + reader + ".js");
-	return toR;
+	return readers[reader];
+	
 }
 
 module.exports.getWriter = getWriter;
 function getWriter(writer) {
-	// TODO loading arbitrary JS!
-	var toR = require("../write/" + writer + ".js");
-	return toR;
+	return writers[writer];
 }
