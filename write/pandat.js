97004aa41e6bb95d0dbef7a2ca9443508dcb5d7f
Ryan Marcus <ryan@rmarcus.info>
2015-11-14 19:06:02 -0500

    added new data source and pandat output

---
+++ write/pandat.js
@@ -0,0 +1,10 @@
+// Copyright 2015 Tiffany Chen, David Giliotti, Ryan Marcus, Eden Zik
+// The file is part of pandat
+
+"use strict";
+
+
+module.exports.convert = convert;
+function convert(ir) {
+	return JSON.stringify(ir);
+}
